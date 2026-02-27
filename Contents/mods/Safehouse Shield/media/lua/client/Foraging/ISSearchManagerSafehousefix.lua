6a1e3acf01cc0fcc81eee94ad9b1851a133364ae
Stefano Scalfari <vorshim92@gmail.com>
2026-02-27 15:24:10 +0100

    ops

--- Contents/mods/Safehouse Shield/media/lua/client/Foraging/ISSearchManagerSafehousefix.lua
+++ Contents/mods/Safehouse Shield/media/lua/client/Foraging/ISSearchManagerSafehousefix.lua
@@ -1,13 +1,9 @@
 local original_ISSearchManager_createIconsForWorldItems = ISSearchManager.createIconsForWorldItems
 function ISSearchManager:createIconsForWorldItems(_square)
-    if isClient() then
-		if not SafeHouse.isSafehouseAllowLoot(_square, self.character) then
-			return true;
-		end;
-	end;
+    if isClient() and SafeHouse.isSafeHouse(_square, getPlayer():getUsername(), true) then
+        if not getServerOptions():getBoolean("SafehouseAllowLoot") then
+            return
+        end;
+    end;
     original_ISSearchManager_createIconsForWorldItems(self, _square)
 end
-
-
-
-
